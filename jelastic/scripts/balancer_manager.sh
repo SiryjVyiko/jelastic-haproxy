67456c770a52f355396672ee711ed8530e7d30e6
jelastic-public-cartridges <jelastic-public-cartridges@hivext.net>
2015-11-25 10:49:42 +0200

    Update balancer_manager.sh

--- jelastic/scripts/balancer_manager.sh
+++ jelastic/scripts/balancer_manager.sh
@@ -7,21 +7,16 @@ function _set_neighbors(){
 }
 
 function rebuildCommon(){
-    su - jelastic -c "${CARTRIDGE_HOME}/versions/$Cartridge_Version/bin/haproxy -D -f ${CARTRIDGE_HOME}/versions/$Cartridge_Version/conf/haproxy.conf -p ${CARTRIDGE_HOME}/run/haproxy.pid -sf $(cat ${CARTRIDGE_HOME}/run/haproxy.pid)"
+    _rebuild_common;
 }
 
 function addCommonHostConfig(){
-    grep -q "${host}:[0-9]* " ${CARTRIDGE_HOME}/versions/$Cartridge_Version/conf/haproxy.conf && return 0;
-    count=$(cat ${CARTRIDGE_HOME}/versions/$Cartridge_Version/conf/haproxy.conf | grep -o "webserver[0-9]*" | sed 's/webserver//g' | sort -n | tail -n1);
-    let "count+=1";
-    sed -i "/backend bk_http ###HOSTS/a\server webserver${count} ${host}:80 cookie S${count} check" /opt/repo/versions/$Cartridge_Version/conf/haproxy.conf;
-    su - jelastic -c "${CARTRIDGE_HOME}/versions/$Cartridge_Version/bin/haproxy -D -f ${CARTRIDGE_HOME}/versions/$Cartridge_Version/conf/haproxy.conf -p ${CARTRIDGE_HOME}/run/haproxy.pid -sf $(cat ${CARTRIDGE_HOME}/run/haproxy.pid)"
+    _add_common_host;
 }
 
 function removeCommonHostConfig(){
-    sed -i '/server.*webserver.*'${host}'/d' ${CARTRIDGE_HOME}/versions/$Cartridge_Version/conf/haproxy.conf;
+    _remove_common_host;
 }
-
 
 function _rebuild_common(){
     su - jelastic -c "${CARTRIDGE_HOME}/versions/$Cartridge_Version/bin/haproxy -D -f ${CARTRIDGE_HOME}/versions/$Cartridge_Version/conf/haproxy.conf -p ${CARTRIDGE_HOME}/run/haproxy.pid -sf $(cat ${CARTRIDGE_HOME}/run/haproxy.pid)"
