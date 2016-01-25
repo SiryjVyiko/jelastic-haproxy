3a4fb481da0ad0cea33ced06198c5c3baf118331
Dmitry Zubelevich <dz@jelastic.com>
2016-01-25 10:17:59 +0200

    binary recompiled

--- jelastic/scripts/balancer_manager.sh
+++ jelastic/scripts/balancer_manager.sh
@@ -7,15 +7,15 @@ function _set_neighbors(){
 }
 
 function _rebuild_common(){
-    su - jelastic -c "${CARTRIDGE_HOME}/versions/$Cartridge_Version/bin/haproxy -D -f ${CARTRIDGE_HOME}/versions/$Cartridge_Version/conf/haproxy.conf -p ${CARTRIDGE_HOME}/run/haproxy.pid -sf $(cat ${CARTRIDGE_HOME}/run/haproxy.pid)"
+    su - jelastic -c "${CARTRIDGE_HOME}/versions/$Version/bin/haproxy -D -f ${CARTRIDGE_HOME}/versions/$Version/conf/haproxy.conf -p ${CARTRIDGE_HOME}/run/haproxy.pid -sf $(cat ${CARTRIDGE_HOME}/run/haproxy.pid)"
 }
 
 function _add_common_host(){
     grep -q "${host}:[0-9]* " ${CARTRIDGE_HOME}/versions/$Cartridge_Version/conf/haproxy.conf && return 0;
-    count=$(cat ${CARTRIDGE_HOME}/versions/$Cartridge_Version/conf/haproxy.conf | grep -o "webserver[0-9]*" | sed 's/webserver//g' | sort -n | tail -n1);
+    count=$(cat ${CARTRIDGE_HOME}/versions/$Version/conf/haproxy.conf | grep -o "webserver[0-9]*" | sed 's/webserver//g' | sort -n | tail -n1);
     let "count+=1";
-    sed -i "/backend bk_http ###HOSTS/a\server webserver${count} ${host}:80 cookie S${count} check" /opt/repo/versions/$Cartridge_Version/conf/haproxy.conf;
-    su - jelastic -c "${CARTRIDGE_HOME}/versions/$Cartridge_Version/bin/haproxy -D -f ${CARTRIDGE_HOME}/versions/$Cartridge_Version/conf/haproxy.conf -p ${CARTRIDGE_HOME}/run/haproxy.pid -sf $(cat ${CARTRIDGE_HOME}/run/haproxy.pid)"
+    sed -i "/backend bk_http ###HOSTS/a\server webserver${count} ${host}:80 cookie S${count} check" /opt/repo/versions/$Version/conf/haproxy.conf;
+    su - jelastic -c "${CARTRIDGE_HOME}/versions/$Version/bin/haproxy -D -f ${CARTRIDGE_HOME}/versions/$Version/conf/haproxy.conf -p ${CARTRIDGE_HOME}/run/haproxy.pid -sf $(cat ${CARTRIDGE_HOME}/run/haproxy.pid)"
 }
 
 
